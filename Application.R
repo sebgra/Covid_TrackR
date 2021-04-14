d0bc6bb46868a5631938ee64f50f82d3fca509a2
sebgra <sebastiengradit@hotmail.com>
2021-04-14 22:11:06 +0200

    First test Raw Data Table

--- Application.R
+++ Application.R
@@ -2,5 +2,38 @@ library(dplyr)
 library(shiny)
 library(shinythemes)
 library(ggplot2)
+library(DT)
 
 options(shiny.maxRequestSize=30*1024^2)
+
+download.file('https://covid.ourworldindata.org/data/owid-covid-data.csv', 'data.csv')
+
+df<- read.csv('data.csv', header=TRUE, sep =',')
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      DT::dataTableOutput('Full_Data'), #Probelm here
+
+
+    )
+  )
+
+
+
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+
+
+  output$Full_Data <- DT::renderDataTable({DT::datatable(df)})
+
+
+}
+
+shinyApp(ui, server)
